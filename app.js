66fee8a570ad553858f364dcec7f5b7572e62943
michael-123-123 <132714921+michael-123-123@users.noreply.github.com>
2025-09-24 08:10:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- CONFIGURACIÓN DE SUPABASE ---
     const SUPABASE_URL = 'https://peiuznumhjdynbffabyq.supabase.co';
-    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlaXV6bnVtaGpkeW5iZmZhYnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNzI2NDYsImV4cCI6MjA3Mzk0ODY0Nn0.vVU32oYJFucSY9N0yGMwSjkJZuCdaA-nQsVxvMCz8nE';
+    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlaXV6bnVtaGpkeW5iZmZhYnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MTE4NTksImV4cCI6MjA3NDI4Nzg1OX0.T6KloEC3W-fpnaqNYxlNWV0aT4FyzxwPUD0UhcqvuJM';
     const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
     // --- ELEMENTOS DEL DOM ---
@@ -927,4 +927,5 @@ document.addEventListener('DOMContentLoaded', () => {
     Auth.checkSession();
 
 });
+
 
